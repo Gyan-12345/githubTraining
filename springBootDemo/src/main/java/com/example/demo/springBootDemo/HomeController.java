2fe22480ebb026c1b4a1b995dbfd0c04100b0799
GYAN <GYAN@DESKTOP-NVNKDKE.mshome.net>
2024-11-08 23:45:52 +0530

    first commit

---
+++ springBootDemo/src/main/java/com/example/demo/springBootDemo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.springBootDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	HomeService homeService;
+
+	@GetMapping("/springBootDemo")
+	public String hello() {
+		System.out.println("hiiiiiiiiiii");
+
+		return "index";
+
+	}
+
+}
---
+++ springBootDemo/src/main/java/com/example/demo/springBootDemo/HomeService.java
@@ -0,0 +1,12 @@
+package com.example.demo.springBootDemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+	public String homeService(String str) {
+
+		return str;
+	}
+}
