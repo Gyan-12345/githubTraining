189c74eed8ac86977a06dfe3f7573fece2b081f8
GYAN <GYAN@DESKTOP-NVNKDKE.mshome.net>
2024-11-09 00:04:07 +0530

    changes in home controller

--- springBootDemo/src/main/java/com/example/demo/springBootDemo/HomeController.java
+++ springBootDemo/src/main/java/com/example/demo/springBootDemo/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
 	@GetMapping("/springBootDemo")
 	public String hello() {
-		System.out.println("hiiiiiiiiiii");
+		System.out.println("Welcome to index page");
 
 		return "index";
 
